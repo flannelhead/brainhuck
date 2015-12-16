ffd66cc9111cc12616affc14bbff9aec98292943
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2015-12-16 10:10:47 +0200

    Just small improvements

--- src/SimpleParser.hs
+++ src/SimpleParser.hs
@@ -2,7 +2,7 @@
 
 module SimpleParser where
 
-data Parser a = P { runP :: String -> Maybe (a, String) }
+data Parser a = P { runParser :: String -> Maybe (a, String) }
 
 instance Functor Parser where
     fmap :: (a -> b) -> Parser a -> Parser b
@@ -14,7 +14,7 @@ instance Applicative Parser where
 
     (<*>) :: Parser (a -> b) -> Parser a -> Parser b
     P pf <*> P px = P $ \s -> pf s >>=
-                        (\(f, s1) -> px s1 >>= (\(x, s2) -> Just (f x, s2)))
+                        (\(f, s1) -> px s1 >>= (\(x, s2) -> return (f x, s2)))
 
 instance Monoid (Parser a) where
     mempty = P $ const Nothing
